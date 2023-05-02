fbd0a8df308cea782b8c4d786cae6e67eb818082
conor.barry <conor.barry@ucdconnect.ie>
2023-05-02 13:46:45 +0100

    delete problematic files

--- backend/descriptions/src/main/resources/init-schema.sql
+++
@@ -1,4 +0,0 @@
-use descriptions;
-
-INSERT INTO descriptions (id, hotel_link, hotel_location, long_description, short_description, thumbnail)
-VALUES ('test1','http://dublinhotel.com','Dublin','Cheap 3 star hotel in Dublin. ','Cheap hotel in Dublin','http://dublin.com'); 
--- backend/descriptions/src/main/resources/schema-mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS descriptions (
-  descID VARCHAR(100) NOT NULL,
-  thumbnail VARCHAR(100) NOT NULL,
-  hotelLocation VARCHAR(100) NOT NULL,
-  shortDescription VARCHAR(100) NOT NULL,
-  longDescription VARCHAR(100) NOT NULL,
-  hotelLink VARCHAR(50) NOT NULL,
-  publicTransport VARCHAR(50) NOT NULL
-);
