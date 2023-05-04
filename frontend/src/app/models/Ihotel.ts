8ffced49aeba3924a9b96ade6ed672ab41e0f598
hehimself <m.s.2002.maerz.14@gmail.com>
2023-05-04 09:42:26 +0100

    changed frontend to match backend

--- frontend/src/app/models/Ihotel.ts
+++ frontend/src/app/models/Ihotel.ts
@@ -1,9 +1,41 @@
+
 export interface Ihotel {
-  id: number
-  name: string,
-  location: string,
-  thumbnail: string,
-  price: number,
-  short_description: string,
-  rating: number,
+  id: string
+  price: Price
+  rating: Rating
+  description: Description
+}
+
+export interface Price {
+  hotelId: string
+  startDate: string
+  endDate: string
+  price: number
+  bookingId: number
+}
+
+export interface Rating {
+  id: string
+  stars: number
+  reviews: Review[]
+}
+
+export interface Review {
+  hotelId: string
+  reviewerName: string
+  stars: number
+  comment: string
+}
+
+export interface Description {
+  id: string
+  shortDescription: string
+  thumbnail: string
+  images: string[]
+  location: string
+  longDescription: string
+  popularAttractions: string[]
+  facilities: string[]
+  hotelLink: string
+  publicTransport: string[]
 }
