8706d552f6949d60b020c0a515823cdfd902e7e9
DanMcC45 <daniel.mccarthy1@ucdconnect.ie>
2023-05-19 11:36:30 +0000

    Fix mongoDB query

--- backend/price/src/main/java/service/BookingRepository.java
+++ backend/price/src/main/java/service/BookingRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface BookingRepository extends MongoRepository<Booking, String> {
-    @Query("{id:'?0'}")
+    @Query("{'_id' : ?0}")
     Booking findBookingById(String id);
 }
