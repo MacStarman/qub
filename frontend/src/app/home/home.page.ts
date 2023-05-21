ac0776d10d8ddd610491e826f1065a66e51631ad
hehimself <m.s.2002.maerz.14@gmail.com>
2023-05-21 14:59:33 +0200

    fronend finished

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { IonicModule, IonRouterOutlet } from '@ionic/angular';
 import { Ihotel } from '../models/Ihotel';
+import { IRequest } from '../models/IRequest';
 import { DataService } from '../services/data.service';
 import { HotelCardComponent } from '../shared/hotel-card/hotel-card.component';
 import { SearchbarComponent } from '../util/searchbar/searchbar.component';
@@ -15,13 +16,29 @@ import { SearchbarComponent } from '../util/searchbar/searchbar.component';
   imports: [IonicModule, SearchbarComponent, HotelCardComponent, CommonModule, RouterModule, HttpClientModule],
 })
 export class HomePage implements OnInit {
-  handleSearchTriggered() {
-    this.show = true;
+  handleRequest(request: IRequest) {
+    // Handle the request here
+    console.log('Received request in parent component:', request);
+    // You can pass the request to other functions or services as needed
+    // Make API POST call with the request in the JSON body
+    this.http.post<Ihotel[]>('http://qube/hotels', request)
+      .subscribe(
+        (response) => {
+          console.log('API response:', response);
+          this.hotels = response;
+          this.show = true;
+        },
+        (error) => {
+          console.error('API error:', error);
+          // Handle the error from the API as needed
+        }
+      );
   }
+
   hotels!: Ihotel[];
   show: boolean = false;
 
-  constructor(private data: DataService, private router: Router) {}
+  constructor(private data: DataService, private router: Router, private http: HttpClient) {}
 
   navigateToHotelDetails(hotel: Ihotel) {
     console.log(hotel.id)
--- frontend/src/app/util/searchbar/searchbar.component.ts
+++ frontend/src/app/util/searchbar/searchbar.component.ts
@@ -12,16 +12,22 @@ import { IRequest } from 'src/app/models/IRequest'
   imports: [IonicModule, FormsModule],
 })
 export class SearchbarComponent implements OnInit {
-  @Output() searchTriggered: EventEmitter<void> = new EventEmitter<void>();
+  @Output() requestSent: EventEmitter<IRequest> = new EventEmitter<IRequest>();
 
-  performSearch(): void {
-    // Perform search logic...
-
-    // Emit the event to notify the parent component
-    this.searchTriggered.emit();
+  startSearch() {
+    const request: IRequest = {
+      id: (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString(),
+      location: this.location,
+      begin_date: this.begin_date,
+      end_date: this.end_date,
+      people: this.people
+    };
+    console.log("Request: " + request.id);
+    this.requestSent.emit(request);
   }
 
   isToastOpen = false;
+
   location!: string;
   begin_date!: Date;
   end_date!: Date;
@@ -31,17 +37,6 @@ export class SearchbarComponent implements OnInit {
     this.isToastOpen = isOpen;
   }
 
-  start_search() {
-    const request: IRequest = {
-      id: (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString(),
-      location: this.location,
-      begin_date: this.begin_date,
-      end_date: this.end_date,
-      people: this.people
-    };
-    console.log("Request: "+request.id)
-    this.data.search(request);
-  }
 
 
   constructor(private data: DataService) { }
