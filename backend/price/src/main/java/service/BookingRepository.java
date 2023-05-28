9a96dfc23c77a39f27ba3cf9e88651d1e4c6bb8d
DanMcC45 <daniel.mccarthy1@ucdconnect.ie>
2023-05-28 13:31:25 +0000

    Delete BookingRepository.java

--- backend/price/src/main/java/service/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-    @Query("{'_id' : ?0}")
-    Booking findBookingById(String id);
-}
