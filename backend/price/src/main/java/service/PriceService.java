1dce2ab0edc64fb1b918964827bf57236776e06d
DanMcC45 <daniel.mccarthy1@ucdconnect.ie>
2023-05-28 13:34:07 +0000

    Clean up PriceService.java

--- backend/price/src/main/java/service/PriceService.java
+++ backend/price/src/main/java/service/PriceService.java
@@ -1,28 +1,8 @@
 package service;
-
 import service.core.BookingForm;
-
 import java.util.concurrent.TimeUnit;
 
-/**
- * Implementation of the AuldFellas insurance quotation service.
- *
- * @author Rem
- *
- */
 public class PriceService{
-    // All references are to be prefixed with an AF (e.g. AF001000)
-    public static final String PREFIX = "AF";
-    public static final String COMPANY = "Auld Fellas Ltd.";
-
-    /**
-     * Quote generation:
-     * 30% discount for being male
-     * 20% increase for being female
-     * 10% discount for males over 50
-     * additional 10% discount for males over 60
-     */
-
     public double computePrice(BookingForm bookingForm, double hotelPrice) {
         long bookedMillies = Math.abs(bookingForm.endDate.getTime() - bookingForm.startDate.getTime());
         long bookedDays = TimeUnit.DAYS.convert(bookedMillies, TimeUnit.MILLISECONDS);
